372afed9d1aa1394ec64302e24f704b89030bff4
Alessio Rugi <alessiorugi95@gmail.com>
2023-12-13 16:14:40 +0100

    creating array and cycle for array pushing

--- js/script.js
+++ js/script.js
@@ -10,8 +10,19 @@ const {createApp} = Vue;
 createApp({
     data() {
         return {
-            
+            //creo l'array in cui depositerò le 10 email
+            listaEmail: [],
         }
     },
-    
+    mounted() {
+        //creo un ciclo che mi permette di ottenere il numero di email richieste grazie all'utilizzo di un API
+        for(let i=0; i < 10; i++) {
+        axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((risposta) => {
+            console.log(risposta.data.response);
+            //aggiungo le email ricevute nell'array sopra citato
+            this.listaEmail.push(risposta.data.response);
+        });
+        };
+        console.log(this.listaEmail);
+    }
 }).mount('#app')
